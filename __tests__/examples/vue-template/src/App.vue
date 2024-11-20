8353785acb3e94a53ef3aad81dd4e8f926fc4e5d
saurabhdaware <saurabhdaware99@gmail.com>
2024-11-20 17:49:35 +0530

    feat: update vue stackblitz example

--- __tests__/examples/vue-template/src/App.vue
+++ __tests__/examples/vue-template/src/App.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
-import "bsky-widget";
+import 'bsky-widget';
 </script>
 
 <template>
-  <bsky-widget handle="vuejs.org"></bsky-widget>
+  <bsky-widget handle="vuejs.org" theme="dim"></bsky-widget>
 </template>
 
 <style scoped>
 bsky-widget {
   min-height: 300px;
   width: 350px;
+  box-shadow: 1px 1px 8px 1px #4ecb93;
 
   /* theme override */
-  --bsky-primary: #41B883;
+  --bsky-primary: #41b883;
   --bsky-primary-hover: #4ecb93;
 }
 </style>
